be9f20f85f8c99cda8e81b2f68961de51bc70253
ryan.hughes97 <ryan.hughes.jp@gmail.com>
2021-01-14 18:53:55 -0800

    + Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,15 @@
 import React from 'react';
 // Page components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <div>
+      <AboutSection />
+      <ServicesSection />
+    </div>
+  );
 };
 
 export default AboutUs;
