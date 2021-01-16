e12ccdc481e840ab63e50e1c905e623c6f1c3719
ryan.hughes97 <ryan.hughes.jp@gmail.com>
2021-01-15 18:04:15 -0800

    + Global Styles & Styles for Services Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,12 @@ import React from 'react';
 import home1 from '../img/home1.png';
 // import styled
 import styled from 'styled-components';
+import {
+  StyledAbout,
+  StyledDescription,
+  StyledImage,
+  StyledHide,
+} from '../styles';
 
 const AboutSection = () => {
   return (
@@ -32,35 +38,5 @@ const AboutSection = () => {
     </StyledAbout>
   );
 };
-
-// Styled Component
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 1rem;
-  font-weight: lighter;
-`;
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const StyledHide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
